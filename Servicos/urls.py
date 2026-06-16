330b0565aef1908d8d0c363ed241c14e4f5eac68
JeanCodee <jeanbenicio1@gmail.com>
2026-06-16 11:20:05 -0300

    views e url das telas

--- Servicos/urls.py
+++ Servicos/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.servicos, name='servicos'),
+    path('', views.servicos, name='servicos'),
+    
+    path('servico1/', views.servico1_view, name='servico1' ),
+    path('servico2/', views.servico2_view, name='servico2')
 ]
--- Servicos/views.py
+++ Servicos/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+def servicos(request):
+    return render(request, 'servicos.html')
+
+# 🔑 ADICIONE ESTA FUNÇÃO:
+def servico1_view(request):
+    return render(request, 'servico1.html')
+
+def servico2_view(request):
+    return render(request, 'servico2.html')
