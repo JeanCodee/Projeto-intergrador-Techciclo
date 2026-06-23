ffc48c8fa1b9acb3ae6c8ddece85b9999b7326bc
vitorialemospimentel-cpu <vitorialemospimentel@gmail.com>
2026-06-23 12:05:33 -0300

    alterações feitas nas telas Trocas/produto1/produto2

--- Trocas/urls.py
+++ Trocas/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.trocas, name='trocas'), # Página principal de trocas
     
-    # 🔑 ROTA PARA O PRODUTO 1:
+    # 🔑 ROTA PARA O PRODUTO 1 e 2:
     path('produto1/', views.produto1_view, name='produto1'),
     path('produto2/', views.produto2_view, name='produto2')
 ]
