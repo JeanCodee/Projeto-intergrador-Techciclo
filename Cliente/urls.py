4ebd5ffc9c7d4d12a616c223bd211cd0907401d1
vitorialemospimentel-cpu <vitorialemospimentel@gemail.com>
2026-06-24 22:34:45 -0300

    desenvolvimento da tela admin

--- Cliente/urls.py
+++ Cliente/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('', views.perfil, name='perfil' )
+     path('', views.perfil, name='perfil' ),
+     path('administrador/',views.administrador, name='administrador')
 ]
