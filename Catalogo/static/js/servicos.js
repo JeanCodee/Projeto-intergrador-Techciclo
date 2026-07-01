8f8e3a125269bf933a4eee48bbeb7861b26bf466
JeanCodee <jeanbenicio1@gmail.com>
2026-07-01 11:26:06 -0300

    criação de filtros na tela de serviços e produtos e alterações para o link do javascript

---
+++ Catalogo/static/js/servicos.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const filtroForm = document.querySelector(".filtro-opcoes-avancadas");
+
+    if (filtroForm) {
+
+        const posicaoSalva = localStorage.getItem("posicaoScrollFiltro");
+        if (posicaoSalva) {
+            window.scrollTo(0, parseInt(posicaoSalva, 10));
+            localStorage.removeItem("posicaoScrollFiltro");
+        }
+
+        filtroForm.querySelectorAll("select").forEach(select => {
+            select.addEventListener("change", function () {
+                localStorage.setItem("posicaoScrollFiltro", window.scrollY);
+            });
+        });
+    }
+});
